d74ef7bf4888d288cd1dc2fcf57b8e558882a973
towens182 <towens@student.dwc.edu>
2016-08-10 21:08:04 -0400

    added something

--- KarelRobot/KarelRobot/Beeper.h
+++ KarelRobot/KarelRobot/Beeper.h
@@ -2,13 +2,18 @@
 ref class Beeper
 {
 private:
+	
+	int row;
+	int col;
 	int num_beepers;
+	bool beeper_visible;
 
 	static System::Drawing::Icon^ beeperIcon = gcnew System::Drawing::Icon("beeper.bmp");
 public:
 	//Initializing constructor
 	Beeper(int row, int col, int num_beepers);
 
-	
+	//array<Int32>^  singleDArray = gcnew array<Int32>(ARRAY_SIZE)
+
 	//create beeper array read from text file beeper 3, 3, 1   row, col, num_beeper
 };
