82499ce557b6318a51c1c60dd2aea29304acc6a4
towens182 <TOwens@student.dwc.edu>
2016-08-11 20:23:21 -0400

    .

--- KarelRobot/KarelRobot/MyForm.h
+++ KarelRobot/KarelRobot/MyForm.h
@@ -45,7 +45,7 @@ namespace KarelRobot {
 
 	private:
 
-		array <Beeper^, 1>^ beeper_location;
+	
 		Graphics^ g;
 		Pen^ blackPen;
 	private: System::Windows::Forms::Button^  U_but;
@@ -237,34 +237,11 @@ namespace KarelRobot {
 		g = pictureBox1->CreateGraphics();
 		blackPen = gcnew System::Drawing::Pen(Color::Black);
 
-		//Construct beeper_location array
-		beeper_location = gcnew array<Int32>(ARRAY_SIZE);
+		
+
+		
 	}
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
 	}
-	private: System::Void U_but_Click(System::Object^  sender, System::EventArgs^  e) {
-		North = true;
-		West = false;
-		South = false;
-		East = false;
-	}
-	private: System::Void L_but_Click(System::Object^  sender, System::EventArgs^  e) {
-		North = false;
-		West = true;
-		South = false;
-		East = false;
-	}
-	private: System::Void R_but_Click(System::Object^  sender, System::EventArgs^  e) {
-		North = false;
-		West = false;
-		South = false;
-		East = true;
-	}
-	private: System::Void D_but_Click(System::Object^  sender, System::EventArgs^  e) {
-		North = false;
-		West = false;
-		South = true;
-		East = false;
-	}
-};
+	};
 }
