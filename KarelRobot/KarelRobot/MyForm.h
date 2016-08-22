7468cb9268753d9b95523f5141aecf77411b759e
Coler20 <rogerscole9@gmail.com>
2016-08-22 18:51:06 -0400

    .

--- KarelRobot/KarelRobot/MyForm.h
+++ KarelRobot/KarelRobot/MyForm.h
@@ -273,8 +273,7 @@ namespace KarelRobot {
 				//Robot::Robot(twoDArray[i][2], twoDArray[i][3])
 				continue;
 			else if (keywordArray[i] == "Wall")
-				//do something
-				continue;
+				Wall::Wall(twoDArray[i, 0], twoDArray[i, 1]);
 			else if (keywordArray[i] == "Beeper")
 				Beeper::Beeper(twoDArray[i,0], twoDArray[i,1], twoDArray[i,2]);
 		}
